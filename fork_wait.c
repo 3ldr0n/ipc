d1fc347f419443da77861bad74804b1a9caea0d0
Edison Neto <ednetoali@gmail.com>
2019-09-12 20:55:02 -0300

    Create fork_wait.c

---
+++ fork_wait.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv)
+{
+        int pid = fork();
+        if (pid == 0) {
+                printf("Child\n");
+                sleep(100);
+                exit(0);
+        } else if (pid > 0) {
+                int status;
+                //int new_pid;
+                pid_t pid_child;
+                printf("Pai\n");
+                //new_pid = fork();
+                //if (new_pid == 0) exit(0);
+                pid_child = waitpid(pid, &status, WIFEXITED(status));
+                printf("%d\n", status);
+        }
+        return 0;
+}
